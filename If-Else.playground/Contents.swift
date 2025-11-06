3817e25705ba71d2cefd242cc3c88b178891c3cc
Jed <hatdoc@naver.com>
2025-11-06 20:37:14 +0800

    Add files updated

--- If-Else.playground/Contents.swift
+++ If-Else.playground/Contents.swift
@@ -1,3 +1,44 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+let myName = "Vandad"
+let myAge = 20
+let yourName = "Foo"
+let yourAge = 19
+
+if myName == "vandad" {
+    "Your name is \(myName)"
+} else {
+    "Oops, I guessed it wrong"
+}
+
+if myName == "Vandad" {
+    "Now I guessed it correctly"
+} else if myName == "Foo" {
+    "Are you foo?"
+} else {
+    "Okay I give up"
+}
+
+if myName == "Vandad" && myAge == 30 {
+    "Name is Vandad and age is 30"
+} else if myAge == 20 {
+    "I only guessed the ages right"
+} else {
+    " I don't know what I'm doing"
+}
+
+if myAge == 20 || myName == "Foo" {
+    "Either age is 20, name is Foo or both"
+} else if myName == "Vandad" || myAge == 20 {
+    "It's too late to get in this clause"
+}
+
+if myName == "Vandad" && myAge == 22 && yourName == "Foo" || yourAge == 19 {
+    "My name is Vandad and I'm 22 and your name is Foo OR you are 19"
+}
+
+if (myName == "Vandad" && myAge == 22) || (yourName == "Foo" || yourAge == 19) {
+    "My name is Vandad and I'm 22 Or your name is Foo or you are 19"
+} else {
+    "Hmmm, that didn't work so well"
+}
--- Operators.playground/Contents.swift
+++ Operators.playground/Contents.swift
@@ -1,3 +1,32 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+let myAge = 28
+let yourAge = 20
+
+if myAge > yourAge {
+    "I'm older than you"
+}
+else if myAge < yourAge {
+    "I'm younger than you"
+}
+else {
+    "we are the same age"
+}
+
+let myMothersAge = myAge + 30
+
+let doubleMyAge = myAge*2
+
+/// 1. unary prefix
+let foo = !true
+/// 2. unary postfix
+let name = Optional("Vandad")
+type(of: name)
+let unaryPostFix = name!
+type(of: unaryPostFix)
+/// 3. binary infix
+let result = 1 + 2
+/// 4. ternary
+let age = 30
+let message = age >= 18 ? "You are an adult" : "You are not yet an adult"
+
